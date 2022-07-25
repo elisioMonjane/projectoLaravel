b0090bad49185381858efc07da4c6bb8ec8f0627
elisioMonjane <elisio.just.99@gmail.com>
2022-07-25 15:59:10 +0200

    changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>projecto</title>
+<link rel="stylesheet" href="{{('assets/css/style.css') }}"/>
+<link rel="stylesheet" href="bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="text-bg-dark p-3"></div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  active" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  active" href="#">Contact</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>  <!--header-->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/assets/img/slide3.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h1 class="display-5" style="color: rgb(239, 243, 19)">LIFTYmoz</h1>
+                <h3>Quickly build an effective pricing table for your potential customers</h3>
+                <h3>Quickly build an  potential customers</h3>
+
+              <p style="text-transform:uppercase;color:rgb(65, 62, 62)">Some representative placeholder content for the first slide.</p>
+              <button type="button" class="btn btn-warning  btn-lg">Learn More</button>
+              <button type="button" class="btn btn-warning  btn-lg"><i class="fa-solid h-25% fa-arrow-right-to-bracket"></i>  login</button>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="/assets/img/slide1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+
+                <h1 class="display-5" style="color: rgb(243, 247, 9)">LIFTYmoz</h1>
+                <h3>Quickly build an effective pricing table for your potential customers</h3>
+                <h3>Quickly build an  potential customers</h3>
+              <p style="text-transform:uppercase;color:rgb(65, 62, 62">Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="/assets/img/slide5.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h1 class="display-5" style="color: rgb(234, 238, 10)">LIFTYmoz</h1>
+                <h3>Quickly build an effective pricing table for your potential customers</h3>
+                <h3>Quickly build an  potential customers</h3>
+
+
+
+              <p style="text-transform:uppercase;color:rgb(65, 62, 62">Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+
+      </div><!--caroussel-->
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"><h3> premium</h3>
+            <div class="col-6 col-md-4"><img src="/assets/img/imagem1.jpg" class="" alt="..." width="275" height="183"></div>
+        </div>
+        <div class="col-sm"><h3> premium</h3>
+          <div class="col-6 col-md-4"><img src="/assets/img/imagem2.jpg" class="" alt="..." width="275" height="183"></div>
+        </div>
+        <div class="col-sm"><h3> premium</h3>
+
+          <div class="col-6 col-md-4"><img src="/assets/img/imagem3.jpg" class="" alt="..." width="275" height="183"></div>
+        </div>
+      </div>
+    </div>
+    <div class="container-1">
+      <div class="row">
+        <div class="col-sm"><p>here you can use your text to organize your footer. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Voluptatum necessitatibus.
+                    </p></div>
+        <div class="col-sm"><p>here you can use your text to organize your footer. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Voluptatum necessitatibus.
+                    </p></div>
+        <div class="col-sm"><p>here you can use your text to organize your footer. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Voluptatum necessitatibus.
+                    </p></div>
+      </div>
+    </div>
+      <!--inittially precing-->
+  <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+    <h6 class="display-4 fw-normal">Pricing</h6>
+
+  </div>
+</header>
+
+<main>
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm border-warning">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal border-warning">Free</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>10 users included</li>
+            <li>2 GB of storage</li>
+            <li>Email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="w-100 btn btn-lg btn-outline-warning">Sign up for free</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm border-warning">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal border-warning">Pro</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>20 users included</li>
+            <li>10 GB of storage</li>
+            <li>Priority email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="w-100 btn btn-lg btn-warning">Get started</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm border-warning">
+        <div class="card-header py-3">
+            <h4 class="my-0 fw-normal border-indigo-500">Enterprise</h4>
+          </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>30 users included</li>
+            <li>15 GB of storage</li>
+            <li>Phone and email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="w-100 btn btn-lg btn-warning">Contact us</button>
+        </div>
+      </div>
+    </div>
+  </div><!-- end pricing-->
+
+ <!--footer-->
+ <div class="container-2">
+ <footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-cmd-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-5">
+                <h6 class="text-uppercase mb-4 font-weight-bold text-warning ">campany Name</h6>
+                <p>here you can use your text to organize your footer. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Voluptatum necessitatibus.
+                    </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-5">
+                <h6 class="text1-uppercase mb-4 font-weight-bold text-warning ">Products</h6>
+                <p>
+                    <a href="#" class="text-white" style="text-decoration: none;">theprovidence</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white" style="text-decoration: none;">criactives</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white" style="text-decoration: none;">honesty</a>
+                </p>
+
+
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-5">
+            <h6 class="text1-uppercase mb-4 font-weight-bold text-warning ">useful links</h6>
+            <p>
+                <a href="#" class="text-white" style="text-decoration: none;">your acount</a>
+            </p>
+            <p>
+                <a href="#" class="text-white" style="text-decoration: none;">beccom afilliatea</a>
+            </p>
+            <p>
+                <a href="#" class="text-white" style="text-decoration: none;">chipping Ratesses</a>
+            </p>
+
+
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5">
+            <h6 class="text1-uppercase mb-4 font-weight-bold text-warning ">contact</h6>
+
+            <p>
+                <i class="fas fa-home mr-3"></i> MAPUTO, MP 2333,us
+            </p>
+            <p>
+                <i class="fas fa-phone mr-3"></i> +258878397905
+            </p>
+            <p>
+                <i class="fas fa-envelope mr-3"><span class="material-icons">
+                 </span></i> elisio.just.99@gmail.com
+            </p>
+
+
+        </div>
+        <hr class="mb-4">
+        <div class="row align-items-centers">
+            <p>&copy;copyright allright reserved by:
+                <a class="#" style="text-decoration:none;">
+                    <strong class="text-warning"> The Providences</strong>
+                </a>
+            </p>
+            <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md-right">
+                    <ul class="list-unstyled list-inline">
+                      <a href="https://facebook/elisioMondlane"><i class="fa-brands fa-facebook"></i></a>
+                        <li class="list-inline-item">
+                          <a href="#"> <i class="fa-solid fa-envelope"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#"> <i class="fa-brands fa-instagram"></i> </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#"><i class="fa-brands fa-twitter"></i> </a>
+                        </li>
+
+                        <li class="list-inline-item">
+                          <a href="#"><i class="fa-brands fa-linkedin"></i> </i> </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#"></i> </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+ </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script></html>
